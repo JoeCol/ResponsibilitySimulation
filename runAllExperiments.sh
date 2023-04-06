1b57d36fcf220dec91e1f87d44013b1ece34e3a4
Joe Collenette <joe.collenette@manchester.ac.uk>
2023-04-06 09:01:04 +0100

    updated run experiment bash file

--- runAllExperiments.sh
+++ runAllExperiments.sh
@@ -1,40 +1,43 @@
-
-
+rm *.class
 javac ResponsibilityGUI.java
 
-SAVE_LOC_N="/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/Manchester/Responsibility/DirtExperiment/Naive/Result/Dirt"
-SAVE_LOC_R="/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/Manchester/Responsibility/DirtExperiment/ResAgents/Result/Dirt"
+SAVE_LOC_N="/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/Manchester/Responsibility/DirtExperiment2/Naive/Result/Dirt"
+SAVE_LOC_R="/Users/user/Library/CloudStorage/OneDrive-TheUniversityofManchester/Manchester/Responsibility/DirtExperiment2/ResAgents/Result/Dirt"
 SIM_STEPS=20000
 
-TOTAL_RUNS=$((201*20))
+rdirtrate[1]=40
+rdirtrate[2]=50
+rdirtrate[3]=60
+rdirtrate[4]=70
+rdirtrate[5]=80
+
+bdirtrate[1]=1000
+bdirtrate[2]=1250
+bdirtrate[3]=1500
+bdirtrate[4]=1750
+bdirtrate[5]=2000
+
+TOTAL_RUNS=$((5*500))
 RUN=0
-for dirt in {400..200}
+for dirt in {1..5}
 do
-    for j in {1..20}
+    for j in {1..500}
     do
         RUN=$(($RUN+1))
         FILE_TO_CHECK=$SAVE_LOC_N$dirt"/DirtLevels_"$j".csv"
-        echo $FILE_TO_CHECK
-        if test -f $FILE_TO_CHECK; then
-            echo exists
-        else
-            echo does not exist
+        if ! test -f $FILE_TO_CHECK; then
             RESULTS_N=$SAVE_LOC_N$dirt"/"
-            java ResponsibilityGUI naive saveLoc $RESULTS_N simSteps $SIM_STEPS dirtInterval $dirt 10 speed 0 nogui&
+            java ResponsibilityGUI naive saveLoc $RESULTS_N simSteps $SIM_STEPS dirtInterval ${rdirtrate[$dirt]} ${bdirtrate[$dirt]} speed 0 nogui&
             
         fi
 
         FILE_TO_CHECK=$SAVE_LOC_R$dirt"/DirtLevels_"$j".csv"
-        echo $FILE_TO_CHECK
-        if test -f $FILE_TO_CHECK; then
-            echo exists
-        else
-            echo does not exist
+        if ! test -f $FILE_TO_CHECK; then
             RESULTS_R=$SAVE_LOC_R$dirt"/"
-            java ResponsibilityGUI saveLoc $RESULTS_R simSteps $SIM_STEPS dirtInterval $dirt 10 speed 0 nogui& 
+            java ResponsibilityGUI saveLoc $RESULTS_R simSteps $SIM_STEPS dirtInterval ${rdirtrate[$dirt]} ${bdirtrate[$dirt]} speed 0 nogui& 
                    
         fi
         wait
-        echo $RUN of $TOTAL_RUNS done
+        echo -ne $RUN of $TOTAL_RUNS done\\r
     done
 done
