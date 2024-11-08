59d7064938d7ca54777f4d8e58114c726dabb522
Joe Collenette <jcollenettem@icloud.com>
2024-11-08 10:17:02 +0000

    Updated with Results

--- output/generateDirtOverTime.m
+++ output/generateDirtOverTime.m
@@ -1,73 +1,40 @@
+legendStr = ["0.025% Chance of Dirt Every Step",
+             "0.02% Chance of Dirt Every Step",
+             "0.016% Chance of Dirt Every Step",
+             "0.014% Chance of Dirt Every Step",
+             "0.0125% Chance of Dirt Every Step"];
+
 figure
 hold on
-title("Average Dirt Levels over Time (Naive No Check)");
+title("Average Number of Dirty Tiles (Naive Agents)");
 xlabel("Time");
-ylabel("Dirt Level");
+ylabel("Number of Dirty Tiles");
 ylim([0 25]);
-titleT = 'Scenario %d';
 for cn = 1:size(dirtAppearence,2)
-    titleN = sprintf(titleT,dirtAppearence(cn));
     tmp = naive(naive(:,2)==dirtAppearence(cn),[1,4]);
     toShow = zeros(numOfSimSteps,1);
     for run = 1:numOfRuns
         toShow(:,run) = tmp(tmp(:,1)==run,2);
     end
-    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',titleN);
+    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',legendStr(cn));
 end
 legend;
 hold off
 
-figure
-hold on
-title("Average Bad Dirt Levels over Time (Naive No Check)");
-xlabel("Time");
-ylabel("Dirt Level");
-ylim([0 1]);
-for cn = 1:size(dirtAppearence,2)
-    titleN = sprintf(titleT,dirtAppearence(cn));
-    tmp = naive(naive(:,2)==dirtAppearence(cn),[1,5]);
-    toShow = zeros(numOfSimSteps,1);
-    for run = 1:numOfRuns
-        toShow(:,run) = tmp(tmp(:,1)==run,2);
-    end
-    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',titleN);
-end
-legend;
-hold off
 
 figure
 hold on
-title("Average Dirt Levels over Time (Res)");
+title("Average Number of Dirty Tiles (Responsibility Agents)");
 xlabel("Time");
-ylabel("Dirt Level");
-titleT = 'Scenario %d';
+ylabel("Number of Dirty Tiles");
 ylim([0 25]);
 for cn = 1:size(dirtAppearence,2)
-    titleN = sprintf(titleT,dirtAppearence(cn));
     tmp = res(res(:,2)==dirtAppearence(cn),[1,4]);
     toShow = zeros(numOfSimSteps,1);
     for run = 1:numOfRuns
         toShow(:,run) = tmp(tmp(:,1)==run,2);
     end
-    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',titleN);
+    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',legendStr(cn));
 end
 legend;
-hold off
-
-figure
-hold on
-title("Average Bad Dirt Levels over Time (Res)");
-xlabel("Time");
-ylabel("Dirt Level");
-ylim([0 1]);
-legend;
-for cn = 1:size(dirtAppearence,2)
-    titleN = sprintf(titleT,dirtAppearence(cn));
-    tmp = res(res(:,2)==dirtAppearence(cn),[1,5]);
-    toShow = zeros(numOfSimSteps,1);
-    for run = 1:numOfRuns
-        toShow(:,run) = tmp(tmp(:,1)==run,2);
-    end
-    plot(((mean(toShow,2)./possibleDirtLocations)*100),'DisplayName',titleN);
-end
 hold off
